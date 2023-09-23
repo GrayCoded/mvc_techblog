e835398e996789bb22683d997815715bc7d83d36
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-09-23 13:29:22 -0300

    test

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,16 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Blog } = require('../models');
 
 const userData = require('./userData.json');
+const blogData = require('./blogData.json')
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
+
+    await Blog.bulkCreate(blogData, {
+        individualHooks: true,
+        returning: true,
+    });
 
     await User.bulkCreate(userData, {
         individualHooks: true,
